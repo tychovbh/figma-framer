ae9a4a6db2213bc78a55c8b4af71887b75467a02
Tycho <info@bespokeweb.nl>
2022-11-04 11:06:30 +0100

    fix

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,31 @@
-import React from 'react'
+// import React from 'react'
+//
+// export default function Button({title}) {
+//     // This is a React component containing an Example component
+//     // - Replace <Example /> with your own code
+//     // - Find inspiration: https://www.framer.com/developers/
+//
+//     return <button style={buttonStyle}>{title}</button>
+// }
+//
+// const buttonStyle = {
+//     border: 'none',
+//     backgroundColor: 'green',
+//     color: 'white',
+//     paddingLeft: 20,
+//     paddingRight: 20,
+//     paddingTop: 12,
+//     paddingBottom: 12,
+//     borderRadius: 4,
+// }
+//
+// Button.defaultProps = {
+//     title: 'Click me',
+// }
 
-export default function Button({title}) {
-    // This is a React component containing an Example component
-    // - Replace <Example /> with your own code
-    // - Find inspiration: https://www.framer.com/developers/
 
-    return <button style={buttonStyle}>{title}</button>
-}
+import React from "react"
 
-const buttonStyle = {
-    border: 'none',
-    backgroundColor: 'green',
-    color: 'white',
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingTop: 12,
-    paddingBottom: 12,
-    borderRadius: 4,
-}
-
-Button.defaultProps = {
-    title: 'Click me',
+export function TestComponent() {
+    return React.createElement("div", {}, "Hello World")
 }
