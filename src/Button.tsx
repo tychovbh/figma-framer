77e5080ad3d310d727eec3967b10ace7f08360af
Tycho <info@bespokeweb.nl>
2022-11-04 11:39:26 +0100

    fix

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import styles from "./Button.modules.css";
 
 export function Button({ title = "Title" }) {
-  return <button className={styles.button}>{title}</button>;
+  return <button className={styles.button}>{title}g</button>;
 }
